3958c69151a3c9be2ed3b660e677ee8957ec7043
h.kawai <hironori86@gmail.com>
2021-06-26 19:00:36 +0900

    formに名前入力欄を追加。ページ遷移後にPOST内容が表示されるように修正

--- index.js
+++ index.js
@@ -22,11 +22,16 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
+
+            const qs = require('querystring');
+            const answer = qs.parse(decoded);
+            console.log(answer['name'], answer['yaki-shabu']);
+            
             console.info('[' + now + '] 投稿: ' + decoded);
             res.write(
               '<!DOCTYPE html><html lang="ja"><body><h1>' +
-                decoded +
-                'が投稿されました</h1></body></html>'
+                answer['name'] + 'さんは' + answer['yaki-shabu'] +
+                'に投票しました</h1></body></html>'
             );
             res.end();
           });
