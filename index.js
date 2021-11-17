e6ff7f92ce7b4ff52bb3db683e499484daffd670
吉田稲作 <yoshidanoinasaku@gmail.com>
2021-11-17 19:45:29 +0900

    名前の入力欄を追加

--- index.js
+++ index.js
@@ -18,15 +18,18 @@ const server = http
         let rawData = '';
         req
           .on('data', chunk => {
-            rawData = rawData + chunk;
+            rawData = chunk + rawData;
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
             console.info('[' + now + '] 投稿: ' + decoded);
+
+            const qs = require('querystring');
+            const answer = qs.parse(decoded);
             res.write(
               '<!DOCTYPE html><html lang="ja"><body><h1>' +
-                decoded +
-                'が投稿されました</h1></body></html>'
+                answer['name'] + 'さんは' + answer['yaki-shabu'] +
+                'に投票しました</h1></body></html>'
             );
             res.end();
           });
