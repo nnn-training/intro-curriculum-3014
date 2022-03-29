6cefb58b49fa2f8ba63bc106c9454333a7a3e89d
Susumu Otsuka <susumu.otsuka@gmail.com>
2022-03-29 11:23:30 +0900

    アンケートで名前も入力できるようにする

--- index.js
+++ index.js
@@ -22,11 +22,13 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
-            console.info('[' + now + '] 投稿: ' + decoded);
+            const qs = require('querystring');
+            const answer = qs.parse(rawData);
+            console.info(answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投稿しました');
             res.write(
               '<!DOCTYPE html><html lang="ja"><body><h1>' +
-                decoded +
-                'が投稿されました</h1></body></html>'
+                answer['name'] + 'さんは、' + answer['yaki-shabu'] + 
+                'に投稿しました</h1></body></html>'
             );
             res.end();
           });
