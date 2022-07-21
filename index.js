b5d62947b841f14146c5de61413c1a689cf34623
kuyasawa <k.sawa.tttr@gmail.com>
2022-07-21 12:56:41 +0900

    名前の入力を追加

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const qs = require('querystring');
+            const answer = qs.parse(rawData);
+            const body = answer['name'] + 'さんは' + answer['yaki-shabu'] + 'に投票しました。'
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>
+              ${body}
+              </h1></body></html>`
             );
             res.end();
           });
