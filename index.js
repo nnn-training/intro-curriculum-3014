cd8acc7fcf734f2fc3ede4aa44c5a085d501b396
rwtr4462 <blachlorabit194@gmail.com>
2022-11-18 14:21:37 +0900

    アンケートに投稿者名を追加

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const answer = new URLSearchParams(decoded);
+            console.info(`[${now}] ${answer.get('name')} 投稿: ${answer.get('yaki-shabu')}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${answer.get('name')}さんは${answer.get('yaki-shabu')}に投票しました</h1></body></html>`
             );
             res.end();
           });
