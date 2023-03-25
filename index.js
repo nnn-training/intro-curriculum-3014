5acd404e0d2b4e710e9d5afe00e200edf0de32a2
KmxNnCCZ <mmmm48698062@icloud.com>
2023-03-25 13:03:45 +0900

    名前を入力できるように した

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const answer = new URLSearchParams(decoded);
+            const body = `${answer.get('name')}さんは${answer.get('yaki-shabu')}に投票しました`
+            console.info(`[${now}] ${body}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
             );
             res.end();
           });
