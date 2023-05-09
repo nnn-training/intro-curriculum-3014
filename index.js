647384a8130167c5120f38731901957a0ebc0595
akira ueno <akira_ueno@dwango.co.jp>
2023-05-09 16:42:16 +0900

    add name

--- index.js
+++ index.js
@@ -21,10 +21,13 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const params = new URLSearchParams(rawData);
+            const name = params.get('name');
+            const yakiOrShabu = params.get('yaki-shabu');
+            const message = `${name}さんは${yakiOrShabu}に投稿しました`;
+            console.info(`[${now}] ${message}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${message}</h1></body></html>`
             );
             res.end();
           });
