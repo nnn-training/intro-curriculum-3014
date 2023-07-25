646e023f1c5246d7932f98a6a52cdf5316e352b6
hiroto takao <biiiy080121@outlook.com>
2023-07-26 08:51:13 +0900

    change user can type in own name.

--- index.js
+++ index.js
@@ -21,12 +21,15 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
-            res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
-            );
-            res.end();
+           const decoded = decodeURIComponent(rawData);
+            const answer = new URLSearchParams(decoded);
+            const name = answer.get('game');
+            const yaki = answer.get('yaki-tofu');   
+            console.info(`[${now}] 名前： ${name}`);
+            console.info(`[${now}] 投稿： ${yaki}`);         
+           res.write(
+            `<!DOCTYPE html><html lang="ja"><body><h1>${name}さんは${yaki}に投票しました</h1></body></html>`);
+              res.end();
           });
         break;
       default:
