a3f40d8ef7251e30b9919a209d8660a09b8854da
hiiro-deduction <202639784+hiiro-deduction@users.noreply.github.com>
2025-07-24 19:17:22 +0900

    投稿後に表示されるメッセージを自然な表現へ変更

--- index.js
+++ index.js
@@ -22,9 +22,12 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
+            const params = new URLSearchParams(decoded);
+            const name = params.get('name') || '名無し';
+            const yakiTofu = params.get('yaki-tofu');
             console.info(`[${now}] 投稿: ${decoded}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${name}さんは${yakiTofu}に投稿しました。</h1></body></html>`
             );
             res.end();
           });
