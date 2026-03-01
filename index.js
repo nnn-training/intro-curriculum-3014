c9eb7f2841ff975c08f3b201a68d48ac97ff6568
namiyama <namiyama814@gmail.com>
2026-03-01 09:09:33 +0900

    アンケートの項目に名前を追加

--- index.js
+++ index.js
@@ -21,10 +21,10 @@ const server = http
             rawData += chunk;
           })
           .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const decoded = new URLSearchParams(rawData);
+            console.info(`[${now}] 投稿: name=${decoded.get('name')}, yaki-tofu=${decoded.get('yaki-tofu')}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded.get('name')}さんは${decoded.get('yaki-tofu')}に投稿しました</h1></body></html>`
             );
             res.end();
           });
