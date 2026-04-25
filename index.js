9f85731513dc75b80d67be9efa8ab1607ca0dbbf
Etsuya Matsutani <220541632+etsu74@users.noreply.github.com>
2026-04-26 00:13:37 +0900

    アンケート表示

--- index.js
+++ index.js
@@ -23,9 +23,20 @@ const server = http
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
             console.info(`[${now}] 投稿: ${decoded}`);
+
+            
+            const answer = new URLSearchParams(rawData);
+            const name = answer.get('name');
+            const choice = answer.get('yaki-tofu');
+
+           
+
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${name}さんは${choice}に投稿しました</h1></body></html>`
             );
+
+
+            
             res.end();
           });
         break;
