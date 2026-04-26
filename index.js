14349ad155ba2b86531c3a4e867b4a3cad0cea49
c90c42e5 <kakiysh@gmail.com>
2026-04-26 12:58:42 +0900

    アンケート結果を綺麗な表示に変更

--- index.js
+++ index.js
@@ -2,44 +2,45 @@
 const http = require('node:http');
 const fs = require('node:fs');
 const server = http
-  .createServer((req, res) => {
-    const now = new Date();
-    console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    });
+    .createServer((req, res) => {
+        const now = new Date();
+        console.info(`[${now}] Requested by ${req.socket.remoteAddress}`);
+        res.writeHead(200, {
+            'Content-Type': 'text/html; charset=utf-8'
+        });
 
-    switch (req.method) {
-      case 'GET':
-        const rs = fs.createReadStream('./form.html');
-        rs.pipe(res);
-        break;
-      case 'POST':
-        let rawData = '';
-        req
-          .on('data', chunk => {
-            rawData += chunk;
-          })
-          .on('end', () => {
-            const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
-            res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
-            );
-            res.end();
-          });
-        break;
-      default:
-        break;
-    }
-  })
-  .on('error', e => {
-    console.error(`[${new Date()}] Server Error`, e);
-  })
-  .on('clientError', e => {
-    console.error(`[${new Date()}] Client Error`, e);
-  });
+        switch (req.method) {
+            case 'GET':
+                const rs = fs.createReadStream('./form.html');
+                rs.pipe(res);
+                break;
+            case 'POST':
+                let rawData = '';
+                req
+                    .on('data', chunk => {
+                        rawData += chunk;
+                    })
+                    .on('end', () => {
+                        const answer = new URLSearchParams(rawData);
+                        const body = `${answer.get('name')}さんは${answer.get('yaki-tofu')}に投票しました`;
+                        console.info(`[${now}] ${body}`);
+                        res.write(
+                            `<!DOCTYPE html><html lang="ja"><body><h1>${body}</h1></body></html>`
+                        );
+                        res.end();
+                    });
+                break;
+            default:
+                break;
+        }
+    })
+    .on('error', e => {
+        console.error(`[${new Date()}] Server Error`, e);
+    })
+    .on('clientError', e => {
+        console.error(`[${new Date()}] Client Error`, e);
+    });
 const port = 8000;
 server.listen(port, () => {
-  console.info(`[${new Date()}] Listening on ${port}`);
+    console.info(`[${new Date()}] Listening on ${port}`);
 });
