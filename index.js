f7cbabc4cb04266cbf14f481de0d6c824baa6089
Yume222746 <ymiliang593@gmail.com>
2026-04-28 19:33:34 +0900

    投票に記名を追加し、投票結果を読みやすく変更

--- index.js
+++ index.js
@@ -22,9 +22,10 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
-            console.info(`[${now}] 投稿: ${decoded}`);
+            const answer = new URLSearchParams(decoded)
+            console.info(`[${now}] ${answer.get('name')}, 投稿: ${answer.get('yaki-tofu')}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${answer.get('name')} さんが ${answer.get('yaki-tofu')} に投稿しました</h1></body></html>`
             );
             res.end();
           });
