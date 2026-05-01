7d58f5d7b4b2aca3b558a8e313ccb6e0c4679336
あなたの名前 <>
2026-05-01 18:01:56 +0900

    アンケート結果を整形

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const http = require('node:http');
 const fs = require('node:fs');
+const { URLSearchParams } = require('node:url');
 const server = http
   .createServer((req, res) => {
     const now = new Date();
@@ -22,9 +23,11 @@ const server = http
           })
           .on('end', () => {
             const decoded = decodeURIComponent(rawData);
+            const answer = new URLSearchParams(decoded);
+
             console.info(`[${now}] 投稿: ${decoded}`);
             res.write(
-              `<!DOCTYPE html><html lang="ja"><body><h1>${decoded}が投稿されました</h1></body></html>`
+              `<!DOCTYPE html><html lang="ja"><body><h1>${answer.get('name')}さんは${answer.get('yaki-tofu')}に投稿しました</h1></body></html>`
             );
             res.end();
           });
